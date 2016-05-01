47d7b11195106c66c69d1bc247437b2ebe4c625c
mstya <slavikbr7@gmail.com>
2016-05-01 12:20:03 +0300

    created simple chat with signalR

---
+++ SignalR/SignalR/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalR.Startup))]
+namespace SignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+
+            ConfigureAuth(app);
+        }
+    }
+}
